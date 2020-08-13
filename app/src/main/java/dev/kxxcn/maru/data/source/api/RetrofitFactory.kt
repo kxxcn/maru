90b301323ae523138c9f46b7ee5775b02e1a01de
kxxcn <kxxcn.dev@gmail.com>
2020-08-13 14:34:31 +0900

    Auto stash before cherry pick of "불필요 코드 제거"

--- app/src/main/java/dev/kxxcn/maru/data/source/api/RetrofitFactory.kt
+++ app/src/main/java/dev/kxxcn/maru/data/source/api/RetrofitFactory.kt
@@ -5,25 +5,29 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitBuilder {
+object RetrofitFactory {
 
-    private fun getRetrofit(url: String): Retrofit {
-        val loggingInterceptor = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-
-        val httpClient = OkHttpClient.Builder().apply {
-            addInterceptor(loggingInterceptor)
-        }
+    fun retrofit(url: String): Retrofit {
+        val httpClient = OkHttpClient
+                .Builder()
+                .apply {
+                    HttpLoggingInterceptor()
+                            .apply { level = HttpLoggingInterceptor.Level.BODY }
+                            .also { addInterceptor(it) }
+                }
 
         return Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(httpClient.build())
-            .build()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build()
     }
 
-    fun createNaverService(): NaverService {
-        return getRetrofit(NaverService.URL).create(NaverService::class.java)
+    inline fun <reified T> create(): T {
+        val url = when (T::class) {
+            NaverService::class -> NaverService.URL
+            else -> throw IllegalArgumentException("Unknown service class: ${T::class}")
+        }
+        return retrofit(url).create(T::class.java)
     }
 }
