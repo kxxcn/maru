2ed4ba39492515a2397a7095ee503d880da29981
kxxcn <kxxcn.dev@gmail.com>
2020-11-08 22:42:53 +0900

    불필요 코드 제거

--- app/src/main/java/dev/kxxcn/maru/util/extension/ViewExtension.kt
+++ app/src/main/java/dev/kxxcn/maru/util/extension/ViewExtension.kt
@@ -3,19 +3,32 @@ package dev.kxxcn.maru.util.extension
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.collection.LruCache
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.createBitmap
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import dev.kxxcn.maru.Event
 import dev.kxxcn.maru.R
+import dev.kxxcn.maru.util.preference.PreferenceUtils
+import dev.kxxcn.maru.view.base.Capturable
+import dev.kxxcn.maru.view.home.HomeAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.anko.imageResource
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.wrapContent
@@ -164,3 +177,90 @@ fun View.requestNewSize(width: Int? = null, height: Int? = null) {
 fun View.asTextView() = this as TextView
 
 fun View.asImageView() = this as ImageView
+
+suspend fun RecyclerView.capture(): Bitmap? {
+    var bitmap: Bitmap? = null
+    (adapter as? HomeAdapter)?.let { adapter ->
+        suppressLayout(true)
+
+        var height = 0
+        // 홀더 간 간격
+        val spacingBetweenHolder = 30.px
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+        val cacheSize = maxMemory / 8
+        val cache = LruCache<String, Bitmap>(cacheSize)
+
+        fun isPassedHolder(viewType: Int): Boolean {
+            return when (viewType) {
+                HomeAdapter.TYPE_WELCOME,
+                HomeAdapter.TYPE_BANNER_AD -> true
+                HomeAdapter.TYPE_DAYS -> adapter.items
+                    ?.firstOrNull()
+                    ?.content
+                    ?.hasDays == false
+                else -> false
+            }
+        }
+
+        val widthMeasureSpec =
+            View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY)
+        val heightMeasureSpec =
+            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+
+        val size = adapter.itemCount
+        for (position in 0 until size) {
+            val viewType = adapter.getItemViewType(position)
+            if (isPassedHolder(viewType)) continue
+            val holder = withContext(Dispatchers.Main) {
+                with(adapter) {
+                    createViewHolder(
+                        this@capture,
+                        viewType
+                    ).also { holder ->
+                        onBindViewHolder(holder, position)
+                        (holder as? Capturable)?.capture()
+
+                        holder.itemView.measure(widthMeasureSpec, heightMeasureSpec)
+                        holder.itemView.layout(
+                            0,
+                            0,
+                            holder.itemView.measuredWidth,
+                            holder.itemView.measuredHeight
+                        )
+                    }
+                }
+            }
+
+            cache.put(position.toString(), holder.itemView.drawToBitmap())
+            height += holder.itemView.measuredHeight + spacingBetweenHolder
+        }
+
+        suppressLayout(false)
+
+        createBitmap(measuredWidth, height, Bitmap.Config.ARGB_8888)
+            .also { bitmap = it }
+            .let {
+                val canvas = Canvas(it)
+                val color = if (PreferenceUtils.useDarkMode) {
+                    ContextCompat.getColor(context, R.color.colorPrimaryDarkNight)
+                } else {
+                    Color.WHITE
+                }
+                canvas.drawColor(color)
+
+                // 캔버스 상 다음 홀더가 그려질 높이(y 좌표)
+                var offset = spacingBetweenHolder
+                val paint = Paint()
+
+                for (position in 0 until size) {
+                    cache[position.toString()]?.let { itemView ->
+                        canvas.drawBitmap(itemView, 0f, offset.toFloat(), paint)
+                        offset += itemView.height + spacingBetweenHolder
+                        itemView.recycle()
+                    }
+                }
+            }
+    }
+
+    return bitmap
+}
--- app/src/main/java/dev/kxxcn/maru/view/home/HomeFragment.kt
+++ app/src/main/java/dev/kxxcn/maru/view/home/HomeFragment.kt
@@ -2,19 +2,11 @@ package dev.kxxcn.maru.view.home
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.collection.LruCache
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.createBitmap
-import androidx.core.view.drawToBitmap
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -28,13 +20,13 @@ import dev.kxxcn.maru.databinding.HomeFragmentBinding
 import dev.kxxcn.maru.util.FileUtils
 import dev.kxxcn.maru.util.LinearSpacingDecoration
 import dev.kxxcn.maru.util.REQUEST_CODE_PERMISSION_STORAGE
+import dev.kxxcn.maru.util.extension.capture
 import dev.kxxcn.maru.util.extension.px
 import dev.kxxcn.maru.util.preference.PreferenceUtils
 import dev.kxxcn.maru.view.base.BaseFragment
-import dev.kxxcn.maru.view.base.Capturable
+import dev.kxxcn.maru.view.base.Scrollable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HomeFragment : BaseFragment(), Scrollable {
@@ -176,89 +168,11 @@ class HomeFragment : BaseFragment(), Scrollable {
         viewModel.isLoading(true)
         val context = context ?: return
         showNavigator(false)
-        val parent = binding.contentsList
         lifecycleScope.launch(Dispatchers.Default) {
-            (parent.adapter as? HomeAdapter)?.let { adapter ->
-                parent.suppressLayout(true)
-
-                var height = 0
-                // 홀더 간 간격
-                val spacingBetweenHolder = 30.px
-                val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
-                val cacheSize = maxMemory / 8
-                val cache = LruCache<String, Bitmap>(cacheSize)
-
-                fun isPassedHolder(viewType: Int): Boolean {
-                    return when (viewType) {
-                        HomeAdapter.TYPE_WELCOME,
-                        HomeAdapter.TYPE_BANNER_AD -> true
-                        HomeAdapter.TYPE_DAYS -> adapter.items
-                            ?.firstOrNull()
-                            ?.content
-                            ?.hasDays == false
-                        else -> false
-                    }
-                }
-
-                val widthMeasureSpec =
-                    View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY)
-                val heightMeasureSpec =
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-
-                val size = adapter.itemCount
-                for (position in 0 until size) {
-                    val viewType = adapter.getItemViewType(position)
-                    if (isPassedHolder(viewType)) continue
-                    val holder = withContext(Dispatchers.Main) {
-                        with(adapter) {
-                            createViewHolder(
-                                parent,
-                                viewType
-                            ).also { holder ->
-                                onBindViewHolder(holder, position)
-                                (holder as? Capturable)?.capture()
-
-                                holder.itemView.measure(widthMeasureSpec, heightMeasureSpec)
-                                holder.itemView.layout(
-                                    0,
-                                    0,
-                                    holder.itemView.measuredWidth,
-                                    holder.itemView.measuredHeight
-                                )
-                            }
-                        }
-                    }
-
-                    cache.put(position.toString(), holder.itemView.drawToBitmap())
-                    height += holder.itemView.measuredHeight + spacingBetweenHolder
-                }
-
-                parent.suppressLayout(false)
-
-                val bitmap = createBitmap(parent.measuredWidth, height, Bitmap.Config.ARGB_8888)
-                val canvas = Canvas(bitmap)
-                val color = if (PreferenceUtils.useDarkMode) {
-                    ContextCompat.getColor(context, R.color.colorPrimaryDarkNight)
-                } else {
-                    Color.WHITE
-                }
-                canvas.drawColor(color)
-
-                // 캔버스 상 다음 홀더가 그려질 높이(y 좌표)
-                var offset = spacingBetweenHolder
-                val paint = Paint()
-
-                for (position in 0 until size) {
-                    cache[position.toString()]?.let {
-                        canvas.drawBitmap(it, 0f, offset.toFloat(), paint)
-                        offset += it.height + spacingBetweenHolder
-                        it.recycle()
-                    }
-                }
-
+            binding.contentsList.capture()?.let {
                 FileUtils.saveBitmapToExternalStorage(
                     context,
-                    bitmap,
+                    it,
                     "${System.currentTimeMillis()}_share.png",
                     handleCaptureSuccess(),
                     handleCaptureFailure()
